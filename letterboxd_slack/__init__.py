b1a6a357dd3c7d48ae7182ee75d80028dec2e0ea
Ben Muschol <ben.muschol@airbnb.com>
2020-12-26 08:49:12 -0500

    Args and kwargs

--- letterboxd_slack/__init__.py
+++ letterboxd_slack/__init__.py
@@ -1,6 +1,6 @@
 from letterboxd_slack import letterboxd, slack
 
-def main():
+def main(*args, **kwargs):
     reviews = letterboxd.get_new_reviews()
     for review in reviews:
         slack.notify_review("#ben-test", review)
